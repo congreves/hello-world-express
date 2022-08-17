57e62cfd8508d7637946aa70909e4451279000fd
Sandy Congreve <sandy.congreve@cmeducations.se>
2022-08-17 10:46:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(process.env.MESSAGE_STAGING);
 });
 
 app.listen(PORT, () => {
